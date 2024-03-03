1a88d8de8190b346c953519beeda3e5e347ec84a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-03-04 03:27:40 +0900

    feat: Add Favorite

---
+++ lib/realworld_elixir_phoenix/articles/favorite.ex
@@ -0,0 +1,31 @@
+defmodule RealworldElixirPhoenix.Articles.Favorite do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias RealworldElixirPhoenix.Accounts.User
+  alias RealworldElixirPhoenix.Articles.Article
+
+  @already_exists "ALREADY_EXISTS"
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "favorites" do
+    belongs_to(:user, User, primary_key: true)
+    belongs_to(:article, Article, primary_key: true)
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(favorite, attrs) do
+    favorite
+    |> cast(attrs, [:user_id, :article_id])
+    |> validate_required([:user_id, :article_id])
+    |> foreign_key_constraint(:user_id)
+    |> foreign_key_constraint(:article_id)
+    |> unique_constraint([:user, :article],
+      name: :user_id_article_id_unique_index,
+      message: @already_exists
+    )
+  end
+end
