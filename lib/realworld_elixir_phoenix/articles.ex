dbf03362d24ae0e6014afe02d2dca3098f4f0ece
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-03-25 23:00:00 +0900

    fix: Edit Articles
    - Add article_preload
    - Update list_articles

--- lib/realworld_elixir_phoenix/articles.ex
+++ lib/realworld_elixir_phoenix/articles.ex
@@ -7,6 +7,18 @@ defmodule RealworldElixirPhoenix.Articles do
   alias RealworldElixirPhoenix.Repo
 
   alias RealworldElixirPhoenix.Articles.Article
+  alias RealworldElixirPhoenix.Articles.Favorite
+  alias RealworldElixirPhoenix.Accounts.User
+
+  @default_limit 20
+  @default_offset 0
+
+  def article_preload(article_or_articles) do
+    article_or_articles
+    |> Repo.preload(:author)
+    |> Repo.preload(:favorites)
+    |> Repo.preload(:tagList)
+  end
 
   @doc """
   Returns the list of articles.
@@ -17,8 +29,58 @@ defmodule RealworldElixirPhoenix.Articles do
       [%Article{}, ...]
 
   """
-  def list_articles do
-    Repo.all(Article)
+  def list_articles(params \\ []) do
+    {limit, params} = params |> Keyword.pop(:limit, @default_limit)
+    {offset, params} = params |> Keyword.pop(:offset, @default_offset)
+
+    from(a in Article)
+    |> article_where(params)
+    |> limit(^limit)
+    |> offset(^offset)
+    |> order_by(desc: :inserted_at)
+    |> Repo.all()
+  end
+
+  def article_where(query, []), do: query
+
+  def article_where(query, [{:tag, tag} | rest]) do
+    query
+    |> join(:left, [a], at in assoc(a, :tagList), as: :tags)
+    |> where([tags: tags], tags.name == ^tag)
+    |> article_where(rest)
+  end
+
+  def article_where(query, [{:author, author_name} | rest]) do
+    query
+    |> join(:inner, [a], author in User, as: :author, on: a.author_id == author.id)
+    |> where([author: author], author.username == ^author_name)
+    |> article_where(rest)
+  end
+
+  def article_where(query, [{:favorited, favorited} | rest]) do
+    query
+    |> join(:left, [a], f in Favorite, as: :favorite, on: a.id == f.article_id)
+    |> join(:left, [favorite: f], fu in User, as: :favorite_user, on: f.user_id == fu.id)
+    |> where([favorite_user: fu], fu.username == ^favorited)
+    |> article_where(rest)
+  end
+
+  def article_where(query, [{:user, %User{} = user} | rest]) do
+    artcile_favorite(query, user)
+    |> article_where(rest)
+  end
+
+  def article_where(query, _), do: query
+
+  def artcile_favorite(query, nil), do: query
+
+  def artcile_favorite(query, user) do
+    query
+    |> join(:left, [a], f in Favorite,
+      as: :myfavorite,
+      on: a.id == f.article_id and f.user_id == ^user.id
+    )
+    |> select_merge([myfavorite: f], %{favorited: not is_nil(f)})
   end
 
   @doc """
