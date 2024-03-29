da97d54e306b8bea46d6389d32dc094d0b3a8803
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-03-29 21:30:00 +0900

    fix: Edit fallback_controller.ex

--- lib/realworld_elixir_phoenix_web/controllers/fallback_controller.ex
+++ lib/realworld_elixir_phoenix_web/controllers/fallback_controller.ex
@@ -10,7 +10,28 @@ defmodule RealworldElixirPhoenixWeb.FallbackController do
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
-    |> put_view(html: RealworldElixirPhoenixWeb.ErrorHTML, json: RealworldElixirPhoenixWeb.ErrorJSON)
+    |> put_view(
+      html: RealworldElixirPhoenixWeb.ErrorHTML,
+      json: RealworldElixirPhoenixWeb.ErrorJSON
+    )
+    |> render(:"404")
+  end
+
+  # This clause handles errors returned by Ecto's insert/update/delete.
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(RealworldElixirPhoenixWeb.ChangesetJSON)
+    |> render(:error, changeset: changeset)
+  end
+
+  def call(conn, _) do
+    conn
+    |> put_status(500)
+    |> put_view(
+      html: RealworldElixirPhoenixWeb.ErrorHTML,
+      json: RealworldElixirPhoenixWeb.ErrorJSON
+    )
     |> render(:"404")
   end
 end
