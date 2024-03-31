d1ef70b46c3c1ebcb509cbb257b8fc49f24ee7a9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-03-31 19:03:09 +0900

    fix: Edit article_json.ex

--- lib/realworld_elixir_phoenix_web/controllers/article_json.ex
+++ lib/realworld_elixir_phoenix_web/controllers/article_json.ex
@@ -1,27 +1,53 @@
 defmodule RealworldElixirPhoenixWeb.ArticleJSON do
+  alias RealworldElixirPhoenixWeb.TagJSON
+
   alias RealworldElixirPhoenix.Articles.Article
+  alias RealworldElixirPhoenix.Accounts.User
 
   @doc """
   Renders a list of articles.
   """
   def index(%{articles: articles}) do
-    %{data: for(article <- articles, do: data(article))}
+    %{
+      articles: for(article <- articles, do: data(article)),
+      articlesCount: length(articles)
+    }
   end
 
   @doc """
   Renders a single article.
   """
   def show(%{article: article}) do
-    %{data: data(article)}
+    %{article: data(article)}
   end
 
   defp data(%Article{} = article) do
     %{
-      id: article.id,
-      title: article.title,
       slug: article.slug,
+      title: article.title,
+      description: article.description,
       body: article.body,
-      description: article.description
+      tagList: TagJSON.index(%{tags: article.tagList}),
+      favoritesCount: article.favorites |> Enum.count(),
+      favorited: article.favorited,
+      createdAt:
+        article.inserted_at
+        |> DateTime.truncate(:millisecond)
+        |> DateTime.to_iso8601(:extended, 0),
+      updatedAt:
+        article.updated_at
+        |> DateTime.truncate(:millisecond)
+        |> DateTime.to_iso8601(:extended, 0),
+      author: data(article.author)
+    }
+  end
+
+  defp data(%User{} = author) do
+    %{
+      username: author.username,
+      bio: author.bio,
+      image: author.image,
+      following: author.following
     }
   end
 end
